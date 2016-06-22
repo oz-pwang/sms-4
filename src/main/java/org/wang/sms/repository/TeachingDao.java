40e7e9a0b822a473ca6f69644db130efcd9ceb86
Pin Wang <pin.wang@ozstrategy.com>
2016-06-22 09:56:44 +0800

    add new Associative table with the teacher ，class and subject

---
+++ src/main/java/org/wang/sms/repository/TeachingDao.java
@@ -0,0 +1,10 @@
+package org.wang.sms.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.wang.sms.model.Teaching;
+
+/**
+ * Created by ozintel06 on 16/6/22.
+ */
+public interface TeachingDao extends CrudRepository<Teaching,Long>{
+}
---
+++ src/main/java/org/wang/sms/service/TeachingService.java
@@ -0,0 +1,9 @@
+package org.wang.sms.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by ozintel06 on 16/6/22.
+ */
+public interface TeachingService {
+}
