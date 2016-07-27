b5f091da5f3721a49e229f0dd5fcf6bf15570dae
Pin Wang <pin.wang@ozstrategy.com>
2016-07-27 22:41:36 +0800

    Fixed build error

--- src/main/java/org/wang/sms/controller/LoginAction.java
+++ src/main/java/org/wang/sms/controller/LoginAction.java
@@ -9,10 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import org.wang.sms.command.LoginFormCommand;
-import org.wang.sms.model.User;
-import org.wang.sms.service.UserService;
-import org.wang.sms.until.Constants;
 
 
 /**
