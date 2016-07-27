f56c2fb81f766917fc013b09f29f5d89d8a643c1
Pin Wang <pin.wang@ozstrategy.com>
2016-07-28 00:46:01 +0800

    add access to the login page

--- src/main/java/org/wang/sms/controller/LoginAction.java
+++ src/main/java/org/wang/sms/controller/LoginAction.java
@@ -19,4 +19,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller public class LoginAction {
 
+    @RequestMapping(
+            value  = "/login",
+            method = RequestMethod.GET
+    )
+    public String toLoginView() {
+        return "login";
+    }
+
 }
